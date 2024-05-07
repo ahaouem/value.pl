7683048c5e3a4a963612230fb89be053a8c3a077
SzymonMielecki <58358829+SzymonMielecki@users.noreply.github.com>
2024-05-07 13:18:27 +0200

    CHORE: logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ export default async function HomePage() {
   return (
     <main className="grid min-h-screen text-zinc-950 md:grid-cols-2">
       <div className="flex-1 bg-zinc-100"></div>
-      <Header />
       <div className="flex-1"></div>
     </main>
   );
