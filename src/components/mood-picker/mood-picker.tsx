521f17bdfb8de45a911e6f5a16c986b330bd0c2a
SzymonMielecki <58358829+SzymonMielecki@users.noreply.github.com>
2024-05-07 20:37:05 +0200

    chore: center moodpicker

--- src/components/mood-picker/mood-picker.tsx
+++ src/components/mood-picker/mood-picker.tsx
@@ -11,12 +11,16 @@ export function MoodPicker({
 } & React.HTMLAttributes<HTMLDivElement>) {
   const mood = ["😔", "😞", "😕", "😐", "😃", "😄", "😊"];
   return (
-    <div className={cn("flex w-full flex-row", className)} {...props}>
+    <div
+      className={cn("flex w-full flex-row justify-evenly", className)}
+      {...props}
+    >
       {mood.map((mood, idx) => (
         <button
           onClick={() => onChange(idx)}
+          key={idx}
           className={cn(
-            "transform p-4 text-4xl opacity-70 transition-all hover:scale-110 hover:opacity-90",
+            "transform p-4 text-7xl opacity-70 transition-all hover:scale-110 hover:opacity-90",
             idx === value && "scale-150 opacity-100 hover:scale-150",
           )}
         >
