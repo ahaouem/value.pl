2015d55bb5e35afdd5bff3a92d65d8531d9e513b
davidkupyn <davidkupyn@gmail.com>
2024-05-07 19:03:15 +0200

    feat: add right section to drawer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 import LeftSection from "@/components/left-section";
 import RightSection from "@/components/right-section";
+import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
 
 export default async function HomePage() {
   return (
     <main className="grid min-h-screen text-zinc-950 lg:grid-cols-2">
       <LeftSection />
-      <RightSection />
+      <Drawer>
+        <DrawerTrigger className="h-8">
+          <div className="bg-muted mx-auto h-2 w-[100px] rounded-full" />
+        </DrawerTrigger>
+        <DrawerContent>
+          <RightSection />
+        </DrawerContent>
+      </Drawer>
     </main>
   );
 }
