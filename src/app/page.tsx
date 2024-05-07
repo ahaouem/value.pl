ce9c58254cedfc51822618b016e28b44abec3ff8
Aleksander Haouem <4leeeksha@gmail.com>
2024-05-07 14:54:51 +0200

    chore: merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
-
 import Header from "@/components/Header";
 
 export default async function HomePage() {
   return (
     <main className="grid min-h-screen text-zinc-950 md:grid-cols-2">
-      <div className="flex-1 bg-zinc-100"></div>
-      <div className="flex-1"></div>
+      <div className="flex-1 bg-zinc-100">adadas</div>
+      <div className="flex-1">adasd</div>
     </main>
   );
-
 }
--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-import DateBlock from "./Date/DateBlock";
-import DateTabs from "./Date/DateTabs";
-
-import BlocksLayout from "./Blocks/BlocksLayout";
-
-export { DateBlock, DateTabs, BlocksLayout };
