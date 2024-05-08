c7bcd3ec490ba29a179f5aa8e9c3dda0a826fc69
Aleksander Haouem <4leeeksha@gmail.com>
2024-05-08 04:10:57 +0200

    feat: Poprawna strona tylne koła jazda
    Co-authored-by: Olivier Kobialka <OlivierKobialka@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import LeftSection from "@/components/left-section";
 import RightSection from "@/components/right-section";
-import { Drawer, DrawerContent, DrawerTrigger } from "@/components/ui/drawer";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default async function HomePage() {
   const { userId }: { userId: string | null } = auth();
@@ -18,8 +24,10 @@ export default async function HomePage() {
         <DrawerTrigger className="fixed inset-x-0 bottom-0 h-8 bg-background lg:hidden">
           <div className="mx-auto h-2 w-[100px] rounded-full bg-muted" />
         </DrawerTrigger>
-        <DrawerContent>
-          <RightSection mobile />
+        <DrawerContent className="h-[calc(100vh-1rem)]">
+          <ScrollArea>
+            <RightSection mobile />
+          </ScrollArea>
         </DrawerContent>
       </Drawer>
 
