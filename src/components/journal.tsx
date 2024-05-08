8f13545cffe0551bb8a77e83207faa8f16e0938d
SzymonMielecki <58358829+SzymonMielecki@users.noreply.github.com>
2024-05-08 09:01:43 +0200

    sdkfasl

--- src/components/journal.tsx
+++ src/components/journal.tsx
@@ -1,3 +1,4 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { moods } from "./mood-picker";
 
 export default function Journal({
@@ -10,19 +11,35 @@ export default function Journal({
   tags: string[];
 }) {
   return (
-    <div className="prose dark:prose-invert prose-zinc">
-      <h2>
-        {moods[mood]?.emoji} {moods[mood]?.tooltip}
-      </h2>
-      <p>{dayDesc}</p>
-      <div>Tags:</div>
-      <div>
-        {tags.map((tag) => (
-          <span key={tag} className="rounded-xl bg-emerald-400 px-2 py-1">
-            {tag}
-          </span>
-        ))}
-      </div>
+    <div className="mt-3.5 flex flex-col justify-center space-y-6 p-2 sm:p-8">
+      <Card>
+        <CardHeader>
+          <h2 className="text-2xl">
+            {moods[mood]?.emoji} {moods[mood]?.tooltip}
+          </h2>
+        </CardHeader>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <div>Tags:</div>
+        </CardHeader>
+        <CardContent>
+          {tags.map((tag) => (
+            <span
+              key={tag}
+              className="rounded-xl bg-emerald-200 px-2 py-1 text-emerald-800"
+            >
+              {tag}
+            </span>
+          ))}
+        </CardContent>
+      </Card>
+      <Card>
+        <CardContent>
+          <p>{dayDesc}</p>
+        </CardContent>
+      </Card>
     </div>
   );
 }
